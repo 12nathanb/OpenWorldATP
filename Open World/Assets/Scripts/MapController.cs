ab07ed7012966d5051c80aac622c1e785689c019
Unknown <nathan@thebarnetts.co.uk>
2018-11-16 14:45:21 +0000

    basic loading

---
+++ Open World/Assets/Scripts/MapController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MapController : MonoBehaviour {
+
+    public string currentScene = null;
+    public string nextScene = null;
+    public string PreviousScene = null;
+
+    
+	// Use this for initialization
+	void Start ()
+    {
+      
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(nextScene, LoadSceneMode.Additive);
+        }
+        
+    }
+
+    void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            SceneManager.UnloadSceneAsync(PreviousScene);
+        }
+
+    }
+}
