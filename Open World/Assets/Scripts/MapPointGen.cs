c9e0d4c3b91d3d4f504603e00e1ba8ee032f4808
Unknown <nathan@thebarnetts.co.uk>
2018-11-20 15:36:07 +0000

    Chunks

---
+++ Open World/Assets/Scripts/MapPointGen.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapPointGen : MonoBehaviour {
+
+    public int MapWidth;
+    public int MapHeight;
+    public Transform plane;
+
+    public List<Transform> pointList;
+
+	// Use this for initialization
+	void Start ()
+    {
+      
+		for(int x = 0; x < MapWidth; x++)
+        {
+            for(int y = 0; y < MapHeight; y++)
+            {
+                pointList.Add (Instantiate(plane, new Vector3(x * 10, 0 , y * 10), Quaternion.identity));
+                pointList[x * y].name = "Point" + (x * y);
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Open World/Assets/Scripts/PlayerDetect.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml;
+using System.Xml.Serialization;
+
+public class PlayerDetect : MonoBehaviour {
+
+    public float range;
+
+    private Transform point;
+    private Transform player;
+
+    public GameObject plane;
+
+    public GameObject chunk;
+
+    private bool isActive = false;
+
+    public SaveChunk save;
+
+    private GameObject gm;
+
+    [XmlAttribute("name")]
+    public string name;
+    // Use this for initialization
+    void Start ()
+    {
+        gm = GameObject.FindGameObjectWithTag("GameController");
+        SaveChunk save = gm.GetComponent<SaveChunk>();
+        point = this.transform;
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float dis = Distance();
+
+		if (dis <= range && isActive == false)
+        {
+           chunk = Instantiate(plane, point.position, Quaternion.identity);
+            chunk.name = this.gameObject.name + " Chunk";
+            name = chunk.name;
+            chunk.GetComponent<Renderer>().material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+            save.chunks.Add(chunk);
+            save.Save("D:/OpenWorldATP/Open World/Assets/chunks.txt");
+            isActive = true;
+        }
+
+        if (dis > range && isActive == true)
+        {
+            Destroy(chunk);
+            isActive = false;
+        }
+    }
+
+    private float Distance()
+    {
+        return Vector3.Distance(point.position, player.position);
+    }
+}
